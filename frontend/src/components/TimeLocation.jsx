df34c1408fc7f192fb97525c8b8c05c9e697dc39
nwakwudoloveth <nwakwudoloveth@gmail.com>
2023-03-01 11:27:38 +0100

    add  button components to display current time and location

---
+++ frontend/src/components/TimeLocation.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function TimeLocation() {
+  return (
+    <div>
+    <div className="flex items-centre justify-centre my-6">
+     <div className="text-white  text-xl font-extralight"> 
+     <p>
+        Tuesday, 31 May 2023 | Local time:12:46 PM
+     </p>
+     </div>
+    </div>
+    <div className="flex items-center justify-center my-3">
+    <p className="text-white text-3xl font-medium">
+     Berlin, DE
+    </p>
+
+    </div>
+    </div>
+  )
+}
+
+export default TimeLocation
