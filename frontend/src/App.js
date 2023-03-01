96470eefc7860ccf49fdd64676e2575060c69478
nwakwudoloveth <nwakwudoloveth@gmail.com>
2023-03-01 11:21:54 +0100

    add timeLocation and Detail  display component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import TopButton from "./components/TopButton";
-import Inputs from "./components/Inputs"
-
-
-
-
+import Inputs from "./components/Inputs";
+import TimeLocation from "./components/TimeLocation";
+import Detail from "./components/Detail";
 
 function App() {
   return (
-  <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-  <TopButton />
-  <Inputs />
-  </div>
-  )
-  }
-  
-   export default App;
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButton />
+      <Inputs />
+      <TimeLocation />
+      <Detail />
+    </div>
+  );
+}
+
+export default App;
