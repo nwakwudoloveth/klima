98552caadeabc25b2225cd6ea2bc2d6a886e4405
nwakwudoloveth <nwakwudoloveth@gmail.com>
2023-03-01 11:20:03 +0100

    format prettier

--- frontend/src/components/TopButton.jsx
+++ frontend/src/components/TopButton.jsx
@@ -1,42 +1,37 @@
-import React from 'react'
+import React from "react";
 
 function TopButton() {
-const cities = [
-{
-id:1,
-title: 'London'
-
-},
-{
-    id:2,
-    title: 'Sydney'
-    
+  const cities = [
+    {
+      id: 1,
+      title: "London",
     },
     {
-        id:3,
-        title: 'Tokyo'
-        
-        },
-        {
-            id:4,
-            title: 'Toronto'
-            
-            },
-            {
-                id:5,
-                title: 'Paris'
-                
-                }
-]
+      id: 2,
+      title: "Sydney",
+    },
+    {
+      id: 3,
+      title: "Tokyo",
+    },
+    {
+      id: 4,
+      title: "Toronto",
+    },
+    {
+      id: 5,
+      title: "Paris",
+    },
+  ];
   return (
-
     <div className="flex items-centre justify-around my-6">
-        {cities.map(
-            (city)=> 
-            <button key={city.id} className ="text-white text-lg font-medium">{city.title}</button>
-        )}
+      {cities.map((city) => (
+        <button key={city.id} className="text-white text-lg font-medium">
+          {city.title}
+        </button>
+      ))}
     </div>
-  )
+  );
 }
 
-export default TopButton
+export default TopButton;
