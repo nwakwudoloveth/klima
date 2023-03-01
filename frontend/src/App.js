58cf5efa71fab475b2e79f6cbc30caaf649428a6
nwakwudoloveth <nwakwudoloveth@gmail.com>
2023-03-01 01:53:17 +0100

    add custom App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import TopButton from "./components/TopButton";
+import Inputs from "./components/Inputs"
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+  <TopButton />
+  <Inputs />
+  </div>
+  )
+  }
+  
+   export default App;
